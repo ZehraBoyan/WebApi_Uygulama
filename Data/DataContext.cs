b4afd7eef5ceed742cb96eb16c791f283656c3e9
Zehra Boyan <boyanzehra@gmail.com>
2023-05-30 00:23:43 +0300

    Initial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_Uygulama.Models;
+
+namespace WebApi_Uygulama.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-SIJUH3V;Database=WebApiDb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using WebApi_Uygulama.Data;
+
+namespace WebApi_Uygulama
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<DataContext>(options => {
+                options.UseSqlServer((builder.Configuration.GetConnectionString("ConStr")));
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
